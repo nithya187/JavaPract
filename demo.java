82ddb99771d1e4ad04a550fede1f8cc62a8a5a40
nithya187 <mynithyashree@gmail.com>
2025-05-16 19:08:58 +0800

    added sample java+demo

--- demo.java
+++ demo.java
@@ -2,4 +2,5 @@ public class demo{
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Java Pract");
+        System.out.println("Hello Java ");
 }}
